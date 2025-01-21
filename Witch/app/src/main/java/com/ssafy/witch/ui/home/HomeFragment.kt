5ef49929bd9f4c6d1681752487c727a0a9bbe59d
LimSumi <thddkwl5330@naver.com>
2025-01-22 02:31:49 +0900

    feat: GroupListFragment 템플릿 적용

--- Witch/app/src/main/java/com/ssafy/witch/ui/home/HomeFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/home/HomeFragment.kt
@@ -20,8 +20,6 @@ import java.util.Calendar
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,12 +30,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind
         calendarView.setOnMonthChangedListener { widget, date ->
             val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
 
-            // 현재 달의 날짜는 흰색, 다른 달의 날짜는 회색으로 설정
             if (date.month == currentMonth) {
-                // 현재 달 날짜에 흰색 텍스트 스타일 적용
                 calendarView.setDateTextAppearance(R.style.CustomDateTextWhite)
             } else {
-                // 다른 달 날짜에 회색 텍스트 스타일 적용
                 calendarView.setDateTextAppearance(R.style.CustomDateTextGray)
             }
         }
@@ -54,7 +49,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 

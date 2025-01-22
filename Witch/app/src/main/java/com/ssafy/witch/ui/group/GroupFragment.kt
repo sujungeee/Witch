3896bb7e84e48fbe01b3f870dd5954cf0df26e62
임수미 <thddkwl5330@naver.com>
2025-01-22 14:22:58 +0900

    feat: pull 전 임시저장

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupFragment.kt
@@ -0,0 +1,26 @@
+package com.ssafy.witch.ui.group
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.witch.R
+import com.ssafy.witch.base.BaseFragment
+import com.ssafy.witch.databinding.FragmentGroupBinding
+import com.ssafy.witch.databinding.FragmentGroupListBinding
+
+
+class GroupFragment : BaseFragment<FragmentGroupBinding>(FragmentGroupBinding::bind, R.layout.fragment_group) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}

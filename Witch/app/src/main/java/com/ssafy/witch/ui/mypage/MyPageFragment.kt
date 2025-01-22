b2c7c4d4d26d226cef2a98d0d2d1ea423b19cfd7
임수미 <thddkwl5330@naver.com>
2025-01-22 17:30:06 +0900

    feat: 마이페이지 xml 구현
    MyPageFragment 구현
    
    S12P11D211-106

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,25 @@
+package com.ssafy.witch.ui.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.witch.R
+import com.ssafy.witch.base.BaseFragment
+import com.ssafy.witch.databinding.FragmentGroupEditBinding
+import com.ssafy.witch.databinding.FragmentMyPageBinding
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding::bind, R.layout.fragment_my_page) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}

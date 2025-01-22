0c9b2859c9d11862daad2708662d2c33cb1ef764
임수미 <thddkwl5330@naver.com>
2025-01-22 22:51:09 +0900

    feat: 스낵 조회화면 xml 구현
    SnackFragment xml 구현
    
    S12P11D211-110

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/snack/SnackFragment.kt
@@ -0,0 +1,25 @@
+package com.ssafy.witch.ui.snack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.witch.R
+import com.ssafy.witch.base.BaseFragment
+import com.ssafy.witch.databinding.FragmentPwdEditBinding
+import com.ssafy.witch.databinding.FragmentSnackBinding
+
+
+class SnackFragment : BaseFragment<FragmentSnackBinding>(FragmentSnackBinding::bind, R.layout.fragment_snack) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

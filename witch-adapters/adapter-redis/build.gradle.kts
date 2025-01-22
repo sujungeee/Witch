77f6353e734b7b1be248ef83049722ed93cb63af
김덕윤 <duk9741@gmail.com>
2025-01-22 12:38:44 +0900

    chore: 불필요한 의존성 제거
    - spring boot dependency management 적극 활용

--- witch-adapters/adapter-redis/build.gradle.kts
+++ witch-adapters/adapter-redis/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+
     implementation(project(":witch-cores:core-port"))
     implementation(project(":witch-cores:core-domain"))
     implementation(project(":witch-commons"))
@@ -6,5 +7,4 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-    integrationImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- witch-cores/core-usecase/build.gradle.kts
+++ witch-cores/core-usecase/build.gradle.kts
@@ -3,5 +3,4 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api")
     implementation("io.jsonwebtoken:jjwt-impl")
     implementation("io.jsonwebtoken:jjwt-jackson")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

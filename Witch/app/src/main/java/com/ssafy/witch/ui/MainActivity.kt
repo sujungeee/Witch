995e4552d490c7e2db4ccb21d1b8040fec50798e
임수미 <thddkwl5330@naver.com>
2025-01-22 17:28:38 +0900

    feat: 세부 xml 변경

--- Witch/app/src/main/java/com/ssafy/witch/ui/MainActivity.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.witch.R
 import com.ssafy.witch.databinding.ActivityMainBinding
+import com.ssafy.witch.ui.group.GroupFragment
 import com.ssafy.witch.ui.group.GroupListFragment
 import com.ssafy.witch.ui.home.HomeFragment
-import com.ssafy.witch.ui.map.MapFragment
+import com.ssafy.witch.ui.mypage.MyPageFragment
 
 
 private const val TAG = "MainActivity"
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private val mainBinding : ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,8 +65,8 @@ class MainActivity : AppCompatActivity() {
         when(index){
             1 -> transaction.replace(R.id.main_flayout, HomeFragment())
             2 -> transaction.replace(R.id.main_flayout, GroupListFragment())
-//            3 -> transaction.replace(R.id.main_flayout, //Todo 마이페이지)
-            4 -> transaction.replace(R.id.main_flayout, MapFragment())
+            3 -> transaction.replace(R.id.main_flayout, MyPageFragment())
+//            4 -> transaction.replace(R.id.main_flayout, MapFragment())
 //            5 -> transaction.add(R.id.main_flayout, //Todo)
 //                .addToBackStack(null)
 //            6 -> transaction.add(R.id.main_flayout, //Todo)

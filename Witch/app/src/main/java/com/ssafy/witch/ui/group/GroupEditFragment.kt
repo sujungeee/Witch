d987ebd68cb18851c04aa1dfa881c3564512bbc8
LimSumi <thddkwl5330@naver.com>
2025-01-22 14:05:12 +0900

    feat: 모임 생성/수정 xml 개발
    GroupEditFragment xml 개발
    
    S12P11D211-99

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupEditFragment.kt
@@ -0,0 +1,24 @@
+package com.ssafy.witch.ui.group
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.witch.R
+import com.ssafy.witch.base.BaseFragment
+import com.ssafy.witch.databinding.FragmentGroupEditBinding
+
+class GroupEditFragment : BaseFragment<FragmentGroupEditBinding>(FragmentGroupEditBinding::bind, R.layout.fragment_group_edit) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}

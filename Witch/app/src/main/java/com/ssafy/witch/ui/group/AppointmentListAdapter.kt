90405d001f084f0d715d57f4f2453b4daac714e0
임수미 <thddkwl5330@naver.com>
2025-01-22 22:48:04 +0900

    feat: 모임 상세 약속 리스트 어댑터 구현
    AppointmentListAdapter 구현
    
    S12P11D211-79

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/AppointmentListAdapter.kt
@@ -0,0 +1,53 @@
+package com.ssafy.witch.ui.group
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.witch.data.model.dto.AppointmentListItem
+import com.ssafy.witch.data.model.dto.GroupListItem
+import com.ssafy.witch.data.model.dto.HomeAppointment
+import com.ssafy.witch.databinding.GroupAppointmentListItemBinding
+import com.ssafy.witch.databinding.GroupListItemBinding
+import com.ssafy.witch.databinding.HomeAppointmentListItemBinding
+import kotlin.math.abs
+
+class AppointmentListAdapter(val appointmenrList: List<AppointmentListItem>, val itemClickListener:ItemClickListener) : RecyclerView.Adapter<AppointmentListAdapter.AppointmentListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppointmentListViewHolder {
+        val binding = GroupAppointmentListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AppointmentListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AppointmentListViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = appointmenrList.size
+
+    fun interface ItemClickListener {
+        fun onItemClick(id: Int)
+    }
+
+    inner class AppointmentListViewHolder(private val binding: GroupAppointmentListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("NewApi")
+        fun bind(position: Int) {
+            if (appointmenrList[position].appointmentStatus == "APROVED") {
+                binding.groupAppointmentLiTvIsmine.visibility = ViewGroup.VISIBLE
+            } else {
+                binding.groupAppointmentLiTvIsmine.visibility = ViewGroup.GONE
+            }
+            binding.groupAppointmentLiTvTitle.text = appointmenrList[position].appointmentName
+
+            binding.groupAppointmentLiTvTime.text = appointmenrList[position].appointmentTime.year.toString() + "-" + appointmenrList[position].appointmentTime.monthValue.toString() + "-" + appointmenrList[position].appointmentTime.dayOfMonth.toString()
+            binding.groupAppointmentLiTvDate.text = appointmenrList[position].appointmentTime.hour.toString() + ":" + appointmenrList[position].appointmentTime.minute.toString()
+
+
+
+
+            binding.groupAppointmentListItem.setOnClickListener {
+                itemClickListener.onItemClick(appointmenrList[position].appointmentId)
+            }
+        }
+    }
+}
--- Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.witch.ui.group
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,20 +8,41 @@ import android.view.View
 import android.view.ViewGroup
 import com.ssafy.witch.R
 import com.ssafy.witch.base.BaseFragment
+import com.ssafy.witch.data.model.dto.AppointmentListItem
+import com.ssafy.witch.data.model.dto.GroupListItem
 import com.ssafy.witch.databinding.FragmentGroupBinding
 import com.ssafy.witch.databinding.FragmentGroupListBinding
+import com.ssafy.witch.ui.MainActivity
+import java.time.LocalDateTime
 
 
 class GroupFragment : BaseFragment<FragmentGroupBinding>(FragmentGroupBinding::bind, R.layout.fragment_group) {
-
+    private lateinit var appointmentListAdapter: AppointmentListAdapter
+    private lateinit var appointmentList: List<AppointmentListItem>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
 
 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    @SuppressLint("NewApi")
+    fun initAdapter(){
+        appointmentList = listOf(
+            AppointmentListItem(1, "알고리즘 스터디", LocalDateTime.now(), "APROVED"),
+            AppointmentListItem(2, "맛집탐방",  LocalDateTime.now(), "APROVED"),
+            AppointmentListItem(3, "멋쟁이 알고리즘",  LocalDateTime.now(), "INACTIVE"),
+            AppointmentListItem(4, "알고리즘",  LocalDateTime.now(), "INACTIVE"),
+            AppointmentListItem(5, "멋쟁이",  LocalDateTime.now(), "APROVED")
+        )
+        binding.groupFgRvAppointmentList.adapter = AppointmentListAdapter(appointmentList) { id ->
+            (requireActivity() as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_flayout, GroupFragment()).commit()
+        }
     }
 
 }

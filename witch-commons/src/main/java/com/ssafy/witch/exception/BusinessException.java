fccdea8201c41155876218af3016a8cf68b8ec65
김덕윤 <duk9741@gmail.com>
2025-01-23 13:55:29 +0900

    chore: SQL 테이블 스키마 작성

--- witch-commons/src/main/java/com/ssafy/witch/exception/BusinessException.java
+++ witch-commons/src/main/java/com/ssafy/witch/exception/BusinessException.java
@@ -1,7 +1,9 @@
 package com.ssafy.witch.exception.user;
 
 import com.ssafy.witch.exception.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class UserException extends RuntimeException {
 
   private final ErrorCode errorCode;
@@ -9,4 +11,6 @@ public class UserException extends RuntimeException {
   public UserException(ErrorCode errorCode) {
     this.errorCode = errorCode;
   }
+
+
 }

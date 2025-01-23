2b1a4c835a8c3393fd93a0c47c5335b0699a8191
남수정 <2020113388@dgu.ac.kr>
2025-01-23 15:06:43 +0900

    feat: 모임 가입 신청 xml 개발
    모임 가입 신청 화면 구현
    
    S12P11D211-118

---
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/AppointmentApprovalFragment.kt
@@ -0,0 +1,19 @@
+package com.ssafy.witch.ui.group
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.witch.R
+
+class AppointmentApprovalFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

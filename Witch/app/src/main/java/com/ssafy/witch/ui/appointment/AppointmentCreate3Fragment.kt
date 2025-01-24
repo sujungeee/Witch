a05d58a8cc27e1d434c6fc63272694d5e6134597
남수정 <2020113388@dgu.ac.kr>
2025-01-24 22:18:23 +0900

    feat: 약속 생성 화면-다이얼로그 화면 연결
    약속 생성 화면, 다이얼로그 화면 연결
    
    S12P11D211-123

--- Witch/app/src/main/java/com/ssafy/witch/ui/appointment/AppointmentCreate3Fragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/appointment/AppointmentCreate3Fragment.kt
@@ -1,10 +1,15 @@
 package com.ssafy.witch.ui.appointment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.ssafy.witch.R
 import com.ssafy.witch.base.BaseFragment
 import com.ssafy.witch.databinding.FragmentAppointmentCreate3Binding
+import com.ssafy.witch.ui.MainActivity
 
 class AppointmentCreate3Fragment : BaseFragment<FragmentAppointmentCreate3Binding>(
     FragmentAppointmentCreate3Binding::bind, R.layout.fragment_appointment_create3) {
@@ -15,5 +20,33 @@ class AppointmentCreate3Fragment : BaseFragment<FragmentAppointmentCreate3Bindin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.appointmentFgBtnNext.setOnClickListener {
+            setDialog()
+        }
+    }
+
+    private fun setDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_appointment_create, null)
+        val dialogBuilder = Dialog(requireContext())
+        dialogBuilder.setContentView(dialogView)
+        dialogBuilder.create()
+        dialogBuilder.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialogBuilder.show()
+
+        val appointmentCreateDlBtnYes = dialogView.findViewById<Button>(R.id.dl_btn_yes)
+        val appointmentCreateDlBtnNo = dialogView.findViewById<Button>(R.id.dl_btn_no)
+
+        appointmentCreateDlBtnYes.setOnClickListener {
+            // 약속 생성 가능 시 생성
+
+            showCustomToast("약속이 생성되었어요!")
+            (requireActivity() as MainActivity).openFragment(7)
+            dialogBuilder.dismiss()
+        }
+
+        appointmentCreateDlBtnNo.setOnClickListener {
+            dialogBuilder.dismiss()
+        }
     }
 }

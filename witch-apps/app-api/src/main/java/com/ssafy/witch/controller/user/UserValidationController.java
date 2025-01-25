74b278511b6cd9048dabe040da80228221edff69
김덕윤 <duk9741@gmail.com>
2025-01-25 20:38:11 +0900

    refactor: UserController에서 UserValidationController로 이름 변경

--- witch-apps/app-api/src/main/java/com/ssafy/witch/controller/user/UserValidationController.java
+++ witch-apps/app-api/src/main/java/com/ssafy/witch/controller/user/UserValidationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class UserController {
+public class UserValidationController {
 
   private final ValidateUserUseCase validateUserUseCase;
 

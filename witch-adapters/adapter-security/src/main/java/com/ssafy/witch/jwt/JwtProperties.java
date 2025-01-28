13c9a08a96269238160b9148d5718ab9ec28e326
김덕윤 <duk9741@gmail.com>
2025-01-29 01:14:10 +0900

    feat: RefreshToken 재발급 가능 남은 시간 변수 추가

--- witch-adapters/adapter-security/src/main/java/com/ssafy/witch/jwt/JwtProperties.java
+++ witch-adapters/adapter-security/src/main/java/com/ssafy/witch/jwt/JwtProperties.java
@@ -11,11 +11,14 @@ public class JwtProperties {
   @Value("${witch.jwt.properties.secret}")
   private String secret;
 
-  @Value("${witch.jwt.properties.access-token-expiration-seconds}")
+  @Value("${witch.jwt.properties.access-token.expiration-seconds}")
   private Long accessTokenExpirationSeconds;
 
-  @Value("${witch.jwt.properties.refresh-token-expiration-seconds}")
+  @Value("${witch.jwt.properties.refresh-token.expiration-seconds}")
   private Long refreshTokenExpirationSeconds;
+
+  @Value("${witch.jwt.properties.refresh-token.renew-available-seconds}")
+  private String refreshTokenRenewAvailableSeconds;
 
   @Value("${witch.jwt.properties.token-type}")
   private String tokenType;

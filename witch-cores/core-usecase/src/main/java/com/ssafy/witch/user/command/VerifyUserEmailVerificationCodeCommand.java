21ff62de4fe240e194bdcb3a6a5ec0878bebe6d5
김덕윤 <duk9741@gmail.com>
2025-01-28 16:51:56 +0900

    feat: 기존 Command에 재사용 가능한 validation annotation 적용

--- witch-cores/core-usecase/src/main/java/com/ssafy/witch/user/command/VerifyUserEmailVerificationCodeCommand.java
+++ witch-cores/core-usecase/src/main/java/com/ssafy/witch/user/command/VerifyUserEmailVerificationCodeCommand.java
@@ -1,9 +1,9 @@
 package com.ssafy.witch.user.command;
 
 import com.ssafy.witch.validate.SelfValidating;
+import com.ssafy.witch.validate.annotation.WitchEmailVerificationCode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
@@ -14,7 +14,7 @@ public class VerifyUserEmailVerificationCodeCommand extends
   @Email
   private String email;
 
-  @Pattern(regexp = "^\\d{6}$", message = "이메일 인증 코드는 6자리 숫자여야 합니다.")
+  @WitchEmailVerificationCode
   private String emailVerificationCode;
 
   public VerifyUserEmailVerificationCodeCommand(String email, String emailVerificationCode) {

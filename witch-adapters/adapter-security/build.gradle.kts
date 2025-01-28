8785a7a3ecac23d2bae4fc09891da97a4ab22f9e
김덕윤 <duk9741@gmail.com>
2025-01-28 23:23:04 +0900

    feat: spring security 관련 설정 adapter-security로 이동

--- witch-adapters/adapter-security/build.gradle.kts
+++ witch-adapters/adapter-security/build.gradle.kts
@@ -1,8 +1,9 @@
 dependencies {
-    implementation(project(":witch-cores:core-port"))
-    implementation(project(":witch-cores:core-domain"))
     implementation(project(":witch-commons"))
 
     implementation("org.springframework.boot:spring-boot-starter-logging")
     implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

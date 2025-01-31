f042be2766602d205040bf449ef3c9eb97de0da5
남수정 <2020113388@dgu.ac.kr>
2025-01-31 14:28:41 +0900

    fix: 컴포넌트 id, import fix
    불필요한 import, id fix
    
    S12P11D211-161

--- Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupApprovalFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/group/GroupApprovalFragment.kt
@@ -2,12 +2,9 @@ package com.ssafy.witch.ui.group
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.ssafy.witch.R
 
-class AppointmentApprovalFragment : Fragment() {
+class GroupApprovalFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

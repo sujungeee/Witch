84732bd11ace1520994111b0248b204d13210003
김덕윤 <duk9741@gmail.com>
2025-02-04 15:19:04 +0900

    test: 프로필 이미지 변경 컨트롤러 레이어 테스트

--- witch-apps/app-api/src/test/java/com/ssafy/witch/controller/user/UserProfileImageControllerTest.java
+++ witch-apps/app-api/src/test/java/com/ssafy/witch/controller/user/UserProfileImageControllerTest.java
@@ -2,6 +2,7 @@ package com.ssafy.witch.controller.user;
 
 import static com.ssafy.witch.support.docs.RestDocsUtils.constraints;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
@@ -9,12 +10,15 @@ import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ssafy.witch.controller.user.mapper.UserRequestMapper;
+import com.ssafy.witch.controller.user.request.UpdateProfileImageRequest;
 import com.ssafy.witch.exception.ErrorCode;
+import com.ssafy.witch.exception.file.InvalidFileOwnerException;
 import com.ssafy.witch.exception.file.UnsupportedFileFormatException;
 import com.ssafy.witch.file.PresignedUrl;
 import com.ssafy.witch.file.mapper.FileRequestMapper;
@@ -100,4 +104,54 @@ class UserProfileImageControllerTest extends RestDocsTestSupport {
         .andDo(restDocs.document());
   }
 
+  @WithMockWitchUser
+  @Test
+  void change_profile_image_200() throws Exception {
+
+    UpdateProfileImageRequest request = new UpdateProfileImageRequest(
+        "/profile/profile-image.jpeg");
+
+    mvc.perform(patch("/users/me/profile-image")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .content(objectMapper.writeValueAsString(request))
+        )
+        .andExpect(status().isOk())
+        .andDo(restDocs.document(
+            requestHeaders(
+                headerWithName("Authorization")
+                    .description("JWT access token")
+            ),
+            requestFields(
+                fieldWithPath("objectKey")
+                    .type(STRING)
+                    .description("업로드한 파일의 object key")
+                    .attributes(constraints("확장자가 jpg, jpeg 또는 png여야 합니다"))
+            ),
+            responseFields(
+                fieldWithPath("success")
+                    .type(BOOLEAN)
+                    .description("성공 여부")
+            )
+        ));
+  }
+
+  @WithMockWitchUser
+  @Test
+  void change_profile_image_400_invalid_file_owner() throws Exception {
+
+    UpdateProfileImageRequest request = new UpdateProfileImageRequest(
+        "/profile/profile-image.jpeg");
+
+    doThrow(new InvalidFileOwnerException()).when(profileImageUseCase).updateProfileImageUrl(any());
+
+    mvc.perform(patch("/users/me/profile-image")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .content(objectMapper.writeValueAsString(request))
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("data.errorCode").value(ErrorCode.INVALID_FILE_OWNER.getErrorCode()))
+        .andDo(restDocs.document());
+  }
 }

aa6336c41e5c3ddf86b2798587f27d3ae37c0dea
권경탁 <kkt3289@gmail.com>
2025-02-04 11:18:44 +0900

    fix: JoinService.kt -> AuthService.kt 로 통합 수정
    S12P11D211-199

--- Witch/app/src/main/java/com/ssafy/witch/Auth/JoinFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/Auth/JoinFragment.kt
@@ -1,13 +1,10 @@
-package com.ssafy.witch.login
+package com.ssafy.witch.Auth
 
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
--- Witch/app/src/main/java/com/ssafy/witch/Auth/LoginFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/Auth/LoginFragment.kt
@@ -1,11 +1,8 @@
-package com.ssafy.witch.login
+package com.ssafy.witch.Auth
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.ssafy.witch.R

593b82d0dcb479728c56952f47ea53e569822b81
김덕윤 <duk9741@gmail.com>
2025-02-07 16:52:28 +0900

    test: 내 약속 목록 조회 컨트롤러 레이어 테스트 추가

--- witch-apps/app-api/src/test/java/com/ssafy/witch/controller/appointment/AppointmentReadControllerTest.java
+++ witch-apps/app-api/src/test/java/com/ssafy/witch/controller/appointment/AppointmentReadControllerTest.java
@@ -1,6 +1,7 @@
 package com.ssafy.witch.controller.appointment;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -11,6 +12,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,10 +20,13 @@ import com.ssafy.witch.appointment.AppointmentStatus;
 import com.ssafy.witch.appointment.ReadAppointmentUseCase;
 import com.ssafy.witch.appointment.output.AppointmentListOutput;
 import com.ssafy.witch.appointment.output.AppointmentOutput;
+import com.ssafy.witch.appointment.output.AppointmentWithGroupListOutput;
+import com.ssafy.witch.appointment.output.AppointmentWithGroupOutput;
 import com.ssafy.witch.controller.appointment.mapper.AppointmentResponseMapper;
 import com.ssafy.witch.exception.ErrorCode;
 import com.ssafy.witch.exception.group.GroupNotFoundException;
 import com.ssafy.witch.exception.group.UnauthorizedGroupAccessException;
+import com.ssafy.witch.group.output.GroupOutput;
 import com.ssafy.witch.mock.user.WithMockWitchUser;
 import com.ssafy.witch.support.docs.RestDocsTestSupport;
 import java.time.LocalDateTime;
@@ -134,4 +139,94 @@ class AppointmentReadControllerTest extends RestDocsTestSupport {
         .andDo(restDocs.document());
   }
 
+
+  @WithMockWitchUser
+  @Test
+  void get_my_appointments_200() throws Exception {
+
+    int year = 2030;
+    int month = 1;
+
+    AppointmentWithGroupListOutput output = new AppointmentWithGroupListOutput(
+        List.of(
+            new AppointmentWithGroupOutput("example-appontment-id-1",
+                "약속 이름 1",
+                LocalDateTime.of(2030, 1, 1, 20, 0, 10),
+                AppointmentStatus.SCHEDULED,
+                new GroupOutput("example-group-id-1", "그룹이름1", "http://example.group.image1")),
+            new AppointmentWithGroupOutput("example-appontment-id-1",
+                "약속 이름 1",
+                LocalDateTime.of(2030, 1, 2, 20, 0, 10),
+                AppointmentStatus.SCHEDULED,
+                new GroupOutput("example-group-id-1", "그룹이름2", "http://example.group.image2"))
+        )
+    );
+
+    given(readAppointmentUseCase.getMyAppointments(any(), anyInt(), anyInt())).willReturn(output);
+
+    mvc.perform(get("/appointments/me")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .param("year", String.valueOf(year))
+            .param("month", String.valueOf(month))
+        )
+        .andExpect(status().isOk())
+        .andDo(restDocs.document(
+            queryParameters(
+                parameterWithName("year")
+                    .description("조회하고자 하는 연도"),
+                parameterWithName("month")
+                    .description("조회하고자 하는 월")
+            ),
+            requestHeaders(
+                headerWithName("Authorization")
+                    .description("JWT access token")
+            ),
+            responseFields(
+                fieldWithPath("success")
+                    .type(BOOLEAN)
+                    .description("성공 여부"),
+                fieldWithPath("data.appointments.[].appointmentId")
+                    .type(STRING)
+                    .description("약속 식별자"),
+                fieldWithPath("data.appointments.[].name")
+                    .type(STRING)
+                    .description("약속 이름"),
+                fieldWithPath("data.appointments.[].appointmentTime")
+                    .type(STRING)
+                    .description("약속 시간"),
+                fieldWithPath("data.appointments.[].status")
+                    .type(STRING)
+                    .description("약속 상태(SCHEDULED, ONGOING, FINISHED"),
+                fieldWithPath("data.appointments.[].group.groupId")
+                    .type(STRING)
+                    .description("모임 식별자"),
+                fieldWithPath("data.appointments.[].group.name")
+                    .type(STRING)
+                    .description("모임 이름"),
+                fieldWithPath("data.appointments.[].group.groupImageUrl")
+                    .type(STRING)
+                    .description("모임 사진 URL")
+            )
+        ));
+  }
+
+  @WithMockWitchUser
+  @Test
+  void get_my_appointments_400_invalid_query_params() throws Exception {
+
+    int year = 2000;
+    int month = 13;
+
+    mvc.perform(get("/appointments/me")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .param("year", String.valueOf(year))
+            .param("month", String.valueOf(month))
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("error.errorCode").value(
+            ErrorCode.REQUIRED_FIELD_MISSING_OR_INVALID.getErrorCode()))
+        .andDo(restDocs.document());
+  }
 }

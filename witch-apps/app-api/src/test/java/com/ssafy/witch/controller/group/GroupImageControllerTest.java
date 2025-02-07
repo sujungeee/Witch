4b101ed0e6009a2a0123b589f9b55e24d7f5cb3e
채용수 <chaeyong0320@naver.com>
2025-02-07 15:56:45 +0900

    test: 그룹 사진 변경 기능 테스트 S12P11D211-197

--- witch-apps/app-api/src/test/java/com/ssafy/witch/controller/group/GroupImageControllerTest.java
+++ witch-apps/app-api/src/test/java/com/ssafy/witch/controller/group/GroupImageControllerTest.java
@@ -2,6 +2,7 @@ package com.ssafy.witch.controller.group;
 
 import static com.ssafy.witch.support.docs.RestDocsUtils.constraints;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
@@ -9,12 +10,18 @@ import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ssafy.witch.controller.group.mapper.GroupRequestMapper;
+import com.ssafy.witch.controller.group.request.UpdateGroupImageRequest;
 import com.ssafy.witch.exception.ErrorCode;
+import com.ssafy.witch.exception.file.InvalidFileOwnerException;
 import com.ssafy.witch.exception.file.UnsupportedFileFormatException;
+import com.ssafy.witch.exception.group.GroupNotFoundException;
+import com.ssafy.witch.exception.group.UnauthorizedGroupAccessException;
 import com.ssafy.witch.file.PresignedUrl;
 import com.ssafy.witch.file.mapper.FileRequestMapper;
 import com.ssafy.witch.file.mapper.FileResponseMapper;
@@ -24,12 +31,14 @@ import com.ssafy.witch.mock.user.WithMockWitchUser;
 import com.ssafy.witch.support.docs.RestDocsTestSupport;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
 @WebMvcTest({GroupImageController.class,
-    FileRequestMapper.class, FileResponseMapper.class})
+    FileRequestMapper.class, FileResponseMapper.class,
+    GroupRequestMapper.class})
 class GroupImageControllerTest extends RestDocsTestSupport {
 
   @MockBean
@@ -98,4 +107,96 @@ class GroupImageControllerTest extends RestDocsTestSupport {
         .andDo(restDocs.document());
   }
 
+  @WithMockWitchUser
+  @Test
+  void change_group_image_200() throws Exception {
+
+    String groupId = "test-group-id";
+    UpdateGroupImageRequest request = new UpdateGroupImageRequest("/group/group-image.jpeg");
+
+    mvc.perform(patch("/groups/{groupId}/image", groupId)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer sample.access.token")
+        .content(objectMapper.writeValueAsString(request))
+    )
+        .andExpect(status().isOk())
+        .andDo(restDocs.document(
+            requestHeaders(
+                headerWithName("Authorization")
+                    .description("JWT access token")
+            ),
+            requestFields(
+                fieldWithPath("objectKey")
+                    .type(STRING)
+                    .description("업로드한 파일의 object key, null이면 삭제")
+                    .optional()
+                    .attributes(constraints("확장자가 jpg, jpeg 또는 png여야 합니다."))
+            ),
+            responseFields(
+                fieldWithPath("success")
+                    .type(BOOLEAN)
+                    .description("성공 여부")
+            )
+        ));
+  }
+
+  @WithMockWitchUser
+  @Test
+  void change_group_image_400_group_not_exists() throws Exception {
+
+    String groupId = "test-group-id";
+    UpdateGroupImageRequest request = new UpdateGroupImageRequest("/group/group-image.jpeg");
+
+    doThrow(new GroupNotFoundException())
+        .when(groupImageUseCase).updateGroupImageUrl(any());
+
+    mvc.perform(patch("/groups/{groupId}/image", groupId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .content(objectMapper.writeValueAsString(request))
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("error.errorCode").value(ErrorCode.GROUP_NOT_EXIST.getErrorCode()))
+        .andDo(restDocs.document());
+  }
+
+  @WithMockWitchUser
+  @Test
+  void change_group_image_400_user_is_not_leader() throws Exception{
+
+    String userId = "test-user-id";
+    String groupId = "test-group-id";
+    UpdateGroupImageRequest request = new UpdateGroupImageRequest("/group/group-image.jpeg");
+
+    doThrow(new UnauthorizedGroupAccessException())
+        .when(groupImageUseCase).updateGroupImageUrl(any());
+
+    mvc.perform(patch("/groups/{groupId}/image", groupId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .content(objectMapper.writeValueAsString(request))
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("error.errorCode").value(ErrorCode.UNAUTHORIZED_GROUP_ACCESS.getErrorCode()))
+        .andDo(restDocs.document());
+  }
+
+  @WithMockWitchUser
+  @Test
+  void chang_group_image_400_invalid_file_owner() throws Exception {
+
+    String groupId = "test-group-id";
+    UpdateGroupImageRequest request = new UpdateGroupImageRequest("/group/group-image.jpeg");
+
+    doThrow(new InvalidFileOwnerException())
+        .when(groupImageUseCase).updateGroupImageUrl(any());
+    mvc.perform(patch("/groups/{groupId}/image", groupId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer sample.access.token")
+            .content(objectMapper.writeValueAsString(request))
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("error.errorCode").value(ErrorCode.INVALID_FILE_OWNER.getErrorCode()))
+        .andDo(restDocs.document());
+  }
 }

88fdc95826f463cc957c34faf5852b7aa8c8a0c1
채용수 <chaeyong0320@naver.com>
2025-02-07 14:16:43 +0900

    feat: 모임 사진 변경 기능 구현 S12P11D211-197

--- witch-apps/app-api/src/main/java/com/ssafy/witch/controller/group/GroupImageController.java
+++ witch-apps/app-api/src/main/java/com/ssafy/witch/controller/group/GroupImageController.java
@@ -1,5 +1,7 @@
 package com.ssafy.witch.controller.group;
 
+import com.ssafy.witch.controller.group.mapper.GroupRequestMapper;
+import com.ssafy.witch.controller.group.request.UpdateGroupImageRequest;
 import com.ssafy.witch.file.PresignedUrl;
 import com.ssafy.witch.file.command.GeneratePresignedUrlCommand;
 import com.ssafy.witch.file.mapper.FileRequestMapper;
@@ -7,9 +9,13 @@ import com.ssafy.witch.file.mapper.FileResponseMapper;
 import com.ssafy.witch.file.request.PresignedUrlRequest;
 import com.ssafy.witch.file.response.PresignedUrlResponse;
 import com.ssafy.witch.group.GroupImageUseCase;
+import com.ssafy.witch.group.command.UpdateGroupImageCommand;
 import com.ssafy.witch.response.WitchApiResponse;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +28,7 @@ public class GroupImageController {
 
   private final FileRequestMapper fileRequestMapper;
   private final FileResponseMapper fileResponseMapper;
+  private final GroupRequestMapper groupRequestMapper;
 
   @PostMapping("/groups/group-image/presigned-url")
   public WitchApiResponse<PresignedUrlResponse> generatePresignedUrl(
@@ -33,5 +40,19 @@ public class GroupImageController {
 
     return WitchApiResponse.success(response);
   }
+
+  @PatchMapping("/groups/{groupId}/image")
+  public WitchApiResponse<Void> updateGroupImage(
+      @AuthenticationPrincipal String userId,
+      @PathVariable("groupId") String groupId,
+      @RequestBody UpdateGroupImageRequest request) {
+
+    UpdateGroupImageCommand command = groupRequestMapper.toCommand(userId, groupId, request);
+    groupImageUseCase.updateGroupImageUrl(command);
+
+    return WitchApiResponse.success();
+
+  }
+
 
 }

570dae8fd6af3356f8adb2dd0977951215c40e50
임수미 <thddkwl5330@naver.com>
2025-02-09 22:14:05 +0900

    feat: 스낵 생성 화면, 기능 구현
    스낵 생성 화면 구현
    스낵 텍스트 다이얼로그 구현 - 라디오 버튼 추가
    스낵 음성 녹음 다이얼로그 구현
    텍스트 정렬, 색상 radioButton 상태 관리 구현
    음성 녹음 기능 구현
    갤러리 사진 가져오기 구현(실제 폰 경로 확인 필요) - ImagePicker
    사진 및 음성 s3 업로드 로직 구현(예상)
    
    S12P11D211-226 , S12P11D211-227 , S12P11D211-228 , S12P11D211-229, S12P11D211-230

--- Witch/app/src/main/java/com/ssafy/witch/ui/snack/SnackCreateViewModel.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/snack/SnackCreateViewModel.kt
@@ -1,30 +1,212 @@
 package com.ssafy.witch.ui.snack
 
+import android.content.Context
+import android.graphics.Color
+import android.net.Uri
+import android.util.Log
+import android.view.Gravity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ssafy.witch.R
 import com.ssafy.witch.base.ApplicationClass
 import com.ssafy.witch.data.model.response.MyAppointmentResponse
+import com.ssafy.witch.data.model.response.PresignedUrl
 import com.ssafy.witch.data.remote.RetrofitUtil
 import com.ssafy.witch.data.remote.RetrofitUtil.Companion.appointmentService
+import com.ssafy.witch.data.remote.RetrofitUtil.Companion.groupService
+import com.ssafy.witch.data.remote.RetrofitUtil.Companion.s3Service
+import com.ssafy.witch.data.remote.RetrofitUtil.Companion.snackService
+import com.ssafy.witch.data.remote.RetrofitUtil.Companion.userService
+import com.ssafy.witch.data.remote.S3Service
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class SnackCreateViewModel : ViewModel() {
-    private val _snackText = MutableLiveData<String>()
+    private val _selectedButton = MutableLiveData<Int>(1)
+    val selectedButton: LiveData<Int>
+        get() = _selectedButton
+
+    private val _snackText = MutableLiveData<String>("")
     val snackText: LiveData<String>
         get() = _snackText
 
 
-    private val _photoFile = MutableLiveData<File>()
-    val photoFile: LiveData<File>
+    private val _photoFile = MutableLiveData<Uri?>(null)
+    val photoFile: LiveData<Uri?>
         get() = _photoFile
 
 
-    private val _audioFile = MutableLiveData<File>()
-    val audioFile: LiveData<File>
+    private val _audioFile = MutableLiveData<Uri?>(null)
+    val audioFile: LiveData<Uri?>
         get() = _audioFile
+
+
+    private val _recordState = MutableLiveData<Boolean>(false)
+    val recordState: LiveData<Boolean>
+        get() = _recordState
+
+
+    private val _textColor = MutableLiveData<Int>(R.id.text_color_black)
+    val textColor: LiveData<Int>
+        get() = _textColor
+
+
+    private val _textAlign = MutableLiveData<Int>(R.id.top_start)
+    val textAlign: LiveData<Int>
+        get() = _textAlign
+
+    fun setSnackText(text: String) {
+        _snackText.value = text
+    }
+
+    fun setPhoto(file: Uri?) {
+        _photoFile.value = file
+    }
+
+    fun setAudio(file: Uri?) {
+        _audioFile.value = file
+    }
+
+    fun setSelectedButton(button: Int) {
+        _selectedButton.value = button
+    }
+
+    fun deleteText() {
+        _snackText.value = ""
+    }
+
+    fun deletePhoto() {
+        _photoFile.value = null
+    }
+
+    fun deleteAudio() {
+        _audioFile.value = null
+    }
+
+    fun setRecordState(state: Boolean) {
+        _recordState.value = state
+    }
+
+    fun setTextColor(color: Int) {
+        _textColor.value = color
+    }
+
+    fun setTextAlign(align: Int) {
+        _textAlign.value = align
+    }
+
+    suspend fun uploadSnack(context: Context, uri: Uri) {
+        var audioPresignedUrl = PresignedUrl("", "")
+        runCatching {
+            val imagePresignedUrl = getPresignedUrl("image")
+            if (_audioFile.value != null && _audioFile.value != Uri.EMPTY) {
+                audioPresignedUrl = getPresignedUrl("audio")
+            }
+
+            val response= uploadSnackToS3(imagePresignedUrl, audioPresignedUrl, uri, _audioFile.value, context)
+
+            if (response) {
+                createSnack()
+                Log.d("uploadSnack", "업로드 성공")
+            } else {
+                throw Exception("업로드 실패")
+            }
+
+        }
+
+    }
+
+    suspend fun getPresignedUrl(type: String): PresignedUrl {
+        return suspendCoroutine { continuation ->
+            viewModelScope.launch {
+                runCatching {
+                    when (type) {
+                        "image" -> snackService.getImagePresignedUrl("filename.png")
+                        "audio" -> snackService.getAudioPresignedUrl("filename.png")
+                        else -> {
+                            throw Exception("잘못된 type")
+                        }
+                    }
+                }.onSuccess {
+                    if (it.success) {
+                        val presignedUrl = it.data!!
+                        continuation.resume(presignedUrl)
+                    } else {
+                        continuation.resumeWithException(Exception(it.error.errorMessage))
+                    }
+                }.onFailure {
+                    it.printStackTrace()
+                    continuation.resumeWithException(it)
+                }
+            }
+        }
+    }
+
+
+    suspend fun uploadSnackToS3(imagePresignedUrl: PresignedUrl, audioPresignedUrl: PresignedUrl, imageUri: Uri, audioUri: Uri?, context: Context) :Boolean {
+        var requestBody1 = RequestBody.create("audio/mpeg".toMediaType(), File(context.cacheDir, "upload_audio.mp3"))
+        return suspendCoroutine { continuation ->
+            viewModelScope.launch {
+                val imageFile = getFileFromUri(context, imageUri)
+                val requestBody = imageFile.asRequestBody("image/jpeg".toMediaType())
+
+                if (audioUri != null && audioUri != Uri.EMPTY) {
+                    val audioFile = getFileFromUri(context, audioUri)
+                    requestBody1 = audioFile.asRequestBody("audio/mpeg".toMediaType())
+                }
+
+                runCatching {
+                    s3Service.uploadFile(imagePresignedUrl.presignedUrl, requestBody)
+                    if (audioUri != null && audioUri != Uri.EMPTY) {
+                        s3Service.uploadFile(audioPresignedUrl.presignedUrl, requestBody1)
+                    }
+                }.onSuccess {
+                    continuation.resume(true)
+                }.onFailure {
+                    it.printStackTrace()
+                    continuation.resumeWithException(Exception("업로드 실패"))
+                }
+            }
+        }
+    }
+
+
+    fun getFileFromUri(context: Context, uri: Uri?): File {
+        val inputStream = uri?.let { context.contentResolver.openInputStream(it) }
+        val file = File(context.cacheDir, "upload_image.jpg") // 임시 저장소에 파일 생성
+        inputStream.use { input ->
+            file.outputStream().use { output ->
+                input?.copyTo(output)
+            }
+        }
+        return file
+    }
+
+
+    fun createSnack(){
+        viewModelScope.launch {
+            runCatching {
+                // Todo : snackInfo 생성
+                snackService.createSnack()
+            }.onSuccess {
+                if (it.success) {
+                    Log.d("createSnack", "스낵 생성 성공")
+                } else {
+                    Log.d("createSnack", "스낵 생성 실패")
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
 
 
 }
---
+++ Witch/app/src/main/java/com/ssafy/witch/util/ImagePicker.kt
@@ -0,0 +1,42 @@
+package com.ssafy.witch.util
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+class ImagePicker(
+    private val fragment: Fragment,
+    private val onImageSelected: (Uri) -> Unit
+) {
+    private val requestPermissionLauncher = fragment.registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            openGallery()
+        } else {
+            Toast.makeText(fragment.requireContext(), "권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val pickImageLauncher = fragment.registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let(onImageSelected)
+    }
+
+    fun checkPermissionAndOpenGallery() {
+        if (ContextCompat.checkSelfPermission(fragment.requireContext(), Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED) {
+            openGallery()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        }
+    }
+
+    private fun openGallery() {
+        pickImageLauncher.launch("image/*")
+    }
+}

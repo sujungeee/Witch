c8263db941169bc4c19000e382d46b56f81348aa
남수정 <2020113388@dgu.ac.kr>
2025-02-10 13:57:11 +0900

    feat: 약속 장소 통신 구현
    상세 약속에 대한 통신 구현
    
    S12P11D211-232

--- Witch/app/src/main/java/com/ssafy/witch/ui/appointment/MapFragment.kt
+++ Witch/app/src/main/java/com/ssafy/witch/ui/appointment/MapFragment.kt
@@ -13,17 +13,28 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.location.Location
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -39,18 +50,25 @@ import com.ssafy.witch.data.model.dto.SnackItem
 import com.ssafy.witch.databinding.BottomSheetLayoutBinding
 import com.ssafy.witch.databinding.FragmentMapBinding
 import com.ssafy.witch.ui.ContentActivity
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
+private const val TAG = "MapFragment_Witch"
 class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R.layout.fragment_map),
     OnMapReadyCallback {
         private var userStatus = -1
         private var appointmentStatus = -1
 
-        private val viewModel: MapViewModel by viewModels()
+        private val appointmentViewModel: AppointmentViewModel by activityViewModels()
+        private val mapViewModel: MapViewModel by activityViewModels()
         private lateinit var participantsAdapter: AppointmentDetailParticipantsAdapter
         private lateinit var appointmentSnackAdapter: AppointmentSnackAdatper
         private lateinit var snackList: List<SnackItem>
         private lateinit var participantList: List<AppointmentDetailItem.Participants>
 
+        private lateinit var timer: TimerHandler
         private lateinit var map: GoogleMap
         private lateinit var fusedLocationClient: FusedLocationProviderClient
         private val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -62,6 +80,8 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
         @SuppressLint("ClickableViewAccessibility")
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
+
+            startWorkManager()
 
             snackList= listOf(
                 SnackItem(1, R.drawable.example_snack),
@@ -76,13 +96,13 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
             )
 
             participantList= listOf(
-                AppointmentDetailItem.Participants(1, "홍길동", "dd1", true),
-                AppointmentDetailItem.Participants(2, "권길동", "dd2", false),
-                AppointmentDetailItem.Participants(3, "김길동", "dd3", false),
-                AppointmentDetailItem.Participants(4, "남길동", "dd4", false),
-                AppointmentDetailItem.Participants(5, "임길동", "dd5", false),
-                AppointmentDetailItem.Participants(6, "채길동", "dd6", false),
-                AppointmentDetailItem.Participants(7, "태길동", "dd7", false),
+                AppointmentDetailItem.Participants(1, "홍길동", "dd1", true, false),
+                AppointmentDetailItem.Participants(2, "권길동", "dd2", false, false),
+                AppointmentDetailItem.Participants(3, "김길동", "dd3", false, false),
+                AppointmentDetailItem.Participants(4, "남길동", "dd4", false, false),
+                AppointmentDetailItem.Participants(5, "임길동", "dd5", false, false),
+                AppointmentDetailItem.Participants(6, "채길동", "dd6", false, false),
+                AppointmentDetailItem.Participants(7, "태길동", "dd7", false, false),
             )
         }
 
@@ -90,18 +110,19 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
 
-            val bottomSheetView = view.findViewById<View>(R.id.bottom_sheet_layout)
-            _bottomSheetBinding = BottomSheetLayoutBinding.bind(bottomSheetView)
-
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
             val mapFragment =
                 childFragmentManager.findFragmentById(R.id.map_ac_fg) as SupportMapFragment
             mapFragment.getMapAsync(this)
 
+            val bottomSheetView = view.findViewById<View>(R.id.bottom_sheet_layout)
+            _bottomSheetBinding = BottomSheetLayoutBinding.bind(bottomSheetView)
+
+            initView()
             initAdapter()
 
             appointmentStatus= 1
-            userStatus= 1
+            userStatus= 1 // 1: 약속장 약속 삭제, 2: 약속 모임원 약속 참여 3. 약속 모임원 약속 탈퇴
             showBottomSheetLayout(appointmentStatus)
             setUserStatus(userStatus, appointmentStatus)
 
@@ -125,6 +146,26 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
             bottomSheetBinding.mapFgIvBottomAdd.setOnClickListener { // 스낵 영역 내에 있는 스낵 버튼
                 createSnack(userStatus, appointmentStatus)
             }
+        }
+
+        private fun startWorkManager() {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val workRequest = PeriodicWorkRequestBuilder<LocationWorker>(30, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .build()
+
+            WorkManager.getInstance(requireContext()).enqueueUniquePeriodicWork(
+                "LocationWork",
+                ExistingPeriodicWorkPolicy.KEEP,
+                workRequest
+            )
+        }
+
+        private fun initView() {
+            setTimer()
         }
 
         private fun createSnack(userStatus: Int, appointmentStatus: Int) {
@@ -161,7 +202,9 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
 
             appointmentChangeDlBtnYes.setOnClickListener {
                 when(userStatus) { // TODO
-
+                    1 -> appointmentViewModel.deleteAppointment(1)
+                    2 -> appointmentViewModel.participateAppointment(1)
+                    3 -> appointmentViewModel.leaveAppointment(1)
                 }
                 dialogBuilder.dismiss()
             }
@@ -216,22 +259,6 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
             bottomSheetBinding.mapFgRvBottomMembers.adapter= participantsAdapter
         }
 
-        override fun onMapReady(googleMap: GoogleMap) {
-            map = googleMap
-            requestLocationPermissions()
-        }
-
-        private fun requestLocationPermissions() {
-            if (ContextCompat.checkSelfPermission(requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(),
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE)
-            } else {
-                startLocationUpdates()
-            }
-        }
-
         lateinit var mylocation: Location
         private fun startLocationUpdates() {
             if (ContextCompat.checkSelfPermission(requireContext(),
@@ -243,17 +270,34 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
                         mylocation.latitude = location.latitude
                         mylocation.longitude = location.longitude
                         map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
-
-                        val userMarkerBitmap = getBitmapFromVectorDrawable(requireContext(), R.drawable.marker)
-                        val scaledUserMarker = Bitmap.createScaledBitmap(userMarkerBitmap, 100, 100, false)
-                        map.addMarker(MarkerOptions()
-                            .position(userLocation)
-                            .title("내 위치")
-                            .icon(BitmapDescriptorFactory.fromBitmap(scaledUserMarker)))
                     } else {
                     }
                 }
             }
+        }
+
+        private val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    startLocationUpdates()
+                } else {
+                    Toast.makeText(requireContext(), "위치 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        private fun requestLocationPermissions() {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            } else {
+                startLocationUpdates()
+            }
+        }
+
+        override fun onMapReady(googleMap: GoogleMap) {
+            map = googleMap
+            requestLocationPermissions()
         }
 
         fun getBitmapFromVectorDrawable(context: Context, drawableId: Int) : Bitmap {
@@ -271,21 +315,71 @@ class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R
             return bitmap
         }
 
-        override fun onRequestPermissionsResult(
-            requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-        ) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startLocationUpdates()
-                } else {
-                    Toast.makeText(requireContext(), "위치 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+        override fun onResume() {
+            super.onResume()
+            setTimer()
+        }
+
+        @SuppressLint("SetTextI18n")
+        private fun setTimer() {
+            val dateTimeString = "2025-02-06T16:45"
+            val targetDateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+            val duration = Duration.between(LocalDateTime.now(), targetDateTime).seconds
+            mapViewModel.setRemainderTime(duration)
+            if (duration < 0) { // 이미 끝난 약속
+                binding.mapAcTvRemainderTime.text = "0분 0초"
+                binding.mapAcSbRemainderTime.progress = 0
+            } else if (duration in 0..3600) { // 약속 시간 한 시간 이내
+                timer = TimerHandler()
+                timer.startTimer(duration)
+                mapViewModel.remainderTime.observe(viewLifecycleOwner) { remainingTime ->
+                    binding.mapAcTvRemainderTime.text = remainingTime?.let { parseSeconds(it) }
+                    binding.mapAcSbRemainderTime.progress = remainingTime?.toInt() ?: 0
                 }
+            } else if (duration < 3600 * 24) { // 약속 시간 하루 이내
+                binding.mapAcTvRemainderTime.text = Duration.between(LocalDateTime.now(), targetDateTime).toHours().toString() + "시간"
+                binding.mapAcSbRemainderTime.progress = 3600
+            } else { // 약속 시간 하루 초과
+                binding.mapAcTvRemainderTime.text = Duration.between(LocalDateTime.now(), targetDateTime).abs().toDays().toString() + "일"
+                binding.mapAcSbRemainderTime.progress = 3600
             }
+        }
+    
+        private fun parseSeconds(seconds: Long): String {
+            val minutes = (seconds % 3600) / 60
+            val remainingSeconds = seconds % 60
+            return "${minutes}분 ${remainingSeconds}초"
         }
 
         override fun onDestroyView() {
             super.onDestroyView()
             _bottomSheetBinding = null
+        }
+
+        @SuppressLint("HandlerLeak")
+        inner class TimerHandler : Handler(Looper.getMainLooper()) {
+            private var remainingTime: Long = 0
+            private var isRunning = false
+
+            fun startTimer(initialTime: Long) {
+                isRunning = true
+                remainingTime = initialTime
+                removeCallbacksAndMessages(null)
+                post(timerRunnable)
+            }
+
+            private val timerRunnable = object : Runnable {
+                override fun run() {
+                    if (!isRunning) return
+
+                    if (remainingTime > 0) {
+                        remainingTime -= 1
+                        mapViewModel.setRemainderTime(remainingTime)
+                    }
+
+                    postDelayed(this, 1000)
+                }
+            }
         }
 }

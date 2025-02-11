8338d11897cd36b0b730adf41132bf98c7dc17f8
김덕윤 <duk9741@gmail.com>
2025-02-11 14:49:36 +0900

    feat: FCM 토큰 관련 퍼시스턴트 구현

---
+++ witch-adapters/adapter-persistence/src/main/java/com/ssafy/witch/repository/notification/FcmTokenReadRepository.java
@@ -0,0 +1,24 @@
+package com.ssafy.witch.repository.notification;
+
+import com.ssafy.witch.entity.notification.FcmTokenEntity;
+import com.ssafy.witch.notification.FcmTokenWritePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class FcmTokenReadRepository implements FcmTokenWritePort {
+
+  private final FcmTokenJpaRepository fcmTokenJpaRepository;
+
+  @Override
+  public void deleteFcmToken(String fcmToken) {
+    fcmTokenJpaRepository.deleteByFcmToken(fcmToken);
+  }
+
+
+  @Override
+  public void saveFcmToken(String userid, String fcmToken) {
+    fcmTokenJpaRepository.save(new FcmTokenEntity(null, userid, fcmToken));
+  }
+}

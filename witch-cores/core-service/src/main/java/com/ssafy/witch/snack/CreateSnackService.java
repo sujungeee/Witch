d9b9bc0cf88731b2d6e032b97e0ec548489c4022
채용수 <chaeyong0320@naver.com>
2025-02-14 15:23:53 +0900

    refactor: 스낵 생성시 캐시 사용하도록 변경 S12P11D211-263

--- witch-cores/core-service/src/main/java/com/ssafy/witch/snack/CreateSnackService.java
+++ witch-cores/core-service/src/main/java/com/ssafy/witch/snack/CreateSnackService.java
@@ -2,15 +2,16 @@ package com.ssafy.witch.snack;
 
 import com.ssafy.witch.apoointment.AppointmentMemberPort;
 import com.ssafy.witch.apoointment.AppointmentPort;
-import com.ssafy.witch.appointment.Appointment;
-import com.ssafy.witch.appointment.AppointmentStatus;
-import com.ssafy.witch.exception.appointment.AppointmentNotFoundException;
+import com.ssafy.witch.apoointment.OnGoingAppointmentCachePort;
+import com.ssafy.witch.apoointment.model.AppointmentDetailProjection;
+import com.ssafy.witch.apoointment.model.AppointmentMemberProjection;
 import com.ssafy.witch.exception.appointment.UnauthorizedAppointmentAccessException;
 import com.ssafy.witch.exception.file.InvalidFileOwnerException;
 import com.ssafy.witch.exception.snack.NotOngoingAppointmentException;
 import com.ssafy.witch.file.FileOwnerCachePort;
 import com.ssafy.witch.file.PresignedUrlPort;
 import com.ssafy.witch.snack.command.SnackCreateCommand;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ public class CreateSnackService implements CreateSnackUseCase {
 
   private final FileOwnerCachePort fileOwnerCachePort;
   private final PresignedUrlPort presignedUrlPort;
+  private final OnGoingAppointmentCachePort onGoingAppointmentCachePort;
 
   public void createSnack(SnackCreateCommand command) {
     String userId = command.getUserId();
@@ -34,15 +36,14 @@ public class CreateSnackService implements CreateSnackUseCase {
     String snackImageObjectKey = command.getSnackImageObjectKey();
     String snackSoundObjectKey = command.getSnackSoundObjectKey();
 
-    //존재하는 약속?
-    validateAppointmentExists(appointmentId);
+    AppointmentDetailProjection appointmentDetailProjection = onGoingAppointmentCachePort.get(
+        appointmentId);
 
-    //약속 상태가 ongoing?
-    validateAppointmentStatus(appointmentId);
+    validateAppointmentOngoing(appointmentDetailProjection);
 
-    //해당 약속의 멤버인지?
-    validateAppointmentAuthorization(userId, appointmentId);
+    List<AppointmentMemberProjection> members = appointmentDetailProjection.getMembers();
 
+    validateAppointmentAuthorization(members, userId);
     //파일의 소유권이 있는지?
     validateImageOwnership(userId, snackImageObjectKey);
 
@@ -59,6 +60,26 @@ public class CreateSnackService implements CreateSnackUseCase {
     snackPort.save(newSnack);
   }
 
+  private static void validateAppointmentOngoing(AppointmentDetailProjection appointmentDetailProjection) {
+    if (appointmentDetailProjection == null) {
+      throw new NotOngoingAppointmentException();
+    }
+  }
+
+  private static void validateAppointmentAuthorization(List<AppointmentMemberProjection> members, String userId) {
+    boolean exist = false;
+    for (AppointmentMemberProjection member: members) {
+      if (member.getUserId().equals(userId)) {
+        exist = true;
+        break;
+      }
+    }
+
+    if (!exist) {
+      throw new UnauthorizedAppointmentAccessException();
+    }
+  }
+
   private String getSoundAccessUrl(String snackSoundObjectKey) {
     if (Objects.isNull(snackSoundObjectKey)) {
       return null;
@@ -69,18 +90,6 @@ public class CreateSnackService implements CreateSnackUseCase {
 
   private String getImageAccessUrl(String snackImageObjectKey) {
     return presignedUrlPort.getAccessUrl(snackImageObjectKey);
-  }
-
-  private void validateAppointmentAuthorization(String userId, String appointmentId) {
-    if (!appointmentMemberPort.existsByUserIdAndAppointmentId(userId, appointmentId)) {
-      throw new UnauthorizedAppointmentAccessException();
-    }
-  }
-
-  private void validateAppointmentExists(String appointmentId) {
-    if (!appointmentPort.existsById(appointmentId)) {
-      throw new AppointmentNotFoundException();
-    }
   }
 
   private void validateImageOwnership(String userId, String snackImageObjectKey) {
@@ -101,13 +110,6 @@ public class CreateSnackService implements CreateSnackUseCase {
 
     if (!userId.equals(soundOwnerId)) {
       throw new InvalidFileOwnerException();
-    }
-  }
-
-  private void validateAppointmentStatus(String appointmentId) {
-    Appointment appointment = appointmentPort.findById(appointmentId).get();
-    if (!AppointmentStatus.ONGOING.equals(appointment.getStatus())) {
-      throw new NotOngoingAppointmentException();
     }
   }
 

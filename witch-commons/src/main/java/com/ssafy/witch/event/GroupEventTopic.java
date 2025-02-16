bb3c964102767e46078f77e7f14f15f67aed692e
김덕윤 <duk9741@gmail.com>
2025-02-17 01:38:12 +0900

    feat: 그룹 가입 거절 이벤트 발행 구현

--- witch-commons/src/main/java/com/ssafy/witch/event/GroupEventTopic.java
+++ witch-commons/src/main/java/com/ssafy/witch/event/GroupEventTopic.java
@@ -4,6 +4,7 @@ public class GroupEventTopic {
 
   public final static String GROUP_JOIN_REQUEST = "GROUP_JOIN_REQUEST";
   public static final String GROUP_JOIN_REQUEST_APPROVE = "GROUP_JOIN_REQUEST_APPROVE";
+  public static final String GROUP_JOIN_REQUEST_REJECT = "GROUP_JOIN_REQUEST_REJECT";
 
   private GroupEventTopic() {
   }

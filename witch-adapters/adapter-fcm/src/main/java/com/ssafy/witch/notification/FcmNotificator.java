4296b8cafe00188a720d81980211e6976583545b
김덕윤 <duk9741@gmail.com>
2025-02-17 13:09:52 +0900

    feat: fcm 이미지 삭제

--- witch-adapters/adapter-fcm/src/main/java/com/ssafy/witch/notification/FcmNotificator.java
+++ witch-adapters/adapter-fcm/src/main/java/com/ssafy/witch/notification/FcmNotificator.java
@@ -3,15 +3,11 @@ package com.ssafy.witch.notification;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class FcmNotificator {
-
-  @Value("${witch.fcm.app-icon-url}")
-  private String appIconUrl;
 
   public void sendNotification(WitchNotification witchNotification) {
     try {
